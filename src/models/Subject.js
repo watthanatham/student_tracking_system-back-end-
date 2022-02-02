d82509a9c4d9541c8a816edd666167d4f2be5cf3
Watthanatham Kruram <61160045@go.buu.ac.th>
2022-02-02 15:12:18 +0700

    update subject

--- src/models/Subject.js
+++ src/models/Subject.js
@@ -10,13 +10,49 @@ var Subject = function(subject) {
   this.module_id = subject.module_id
 }
 // get all subjects
-Subject.getAllSubjects = result => {
+Subject.getAllSubjects = (result) => {
   dbCon.query('SELECT * FROM subjects', (err, res) => {
     if(err) {
       console.log('Error while fetching subjects', err)
       result(null, err)
     }else {
       console.log('Subjects fetched successfully')
+      result(null, res)
+    }
+  })
+}
+// get subject by id 
+Subject.getSubjectById = (id, result) => {
+  dbCon.query('SELECT * FROM subjects WHERE sub_id=?', id, (err, res) => {
+    if(err) {
+      console.log('Error while fetching subject by id', err)
+      result(null,err)
+    }else {
+      console.log('Subject fetched by id successfully')
+      result(null,res)
+    }
+  })
+}
+// insert subject
+Subject.createNewSubject = (subjectReqData, result) => {
+  dbCon.query('INSERT INTO subjects SET ?', subjectReqData, (err, res) => {
+    if(err) {
+      console.log('Error while inserting data')
+      result(null,err)
+    }else {
+      console.log('Insert new subject successfully')
+      result(null, res)
+    }
+  })
+}
+// update subject
+Subject.updateSubject = (id, subjectReqData, result) => {
+  dbCon.query('UPDATE subjects SET sub_code=?, sub_name=?, sub_credit=?, st_id=?, module_id=? WHERE sub_id=?', [subjectReqData.sub_code,subjectReqData.sub_name,subjectReqData.sub_credit,subjectReqData.st_id,subjectReqData.module_id, id] , (err, res) => {
+    if(err) {
+      console.log('Error while update data')
+      result(null, err)
+    } else {
+      console.log('Update subject successfully')
       result(null, res)
     }
   })
--- src/routes/subject.js
+++ src/routes/subject.js
@@ -4,5 +4,11 @@ const router = express.Router()
 const subjectController = require('../controllers/SubjectsController')
 // get all subjects
 router.get('/', subjectController.getAllSubjects)
+// get subject by id
+router.get('/:id', subjectController.getSubjectById)
+// create new subject
+router.post('/', subjectController.createNewSubject)
+// update subject
+router.put('/:id', subjectController.updateSubject)
 
 module.exports = router
