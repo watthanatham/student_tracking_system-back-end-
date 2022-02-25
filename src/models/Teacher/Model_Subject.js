f394dc45143ef329e43c37c13f562efa09a0924f
Nutthawut Nualsanga <nutthawutnualsanga@gmail.com>
2022-02-25 15:41:03 +0700

    Edit Module

--- src/models/Teacher/Model_Subject.js
+++ src/models/Teacher/Model_Subject.js
@@ -1,12 +1,12 @@
 var dbCon = require('../../../config/db.config')
 
-var Model_Subject = function(subject) {
+var Model_Subject = function(model_subject) {
   this.sub_code = Model_Subject.sub_code
   this.sub_name_thai = Model_Subject.sub_name_thai
   this.module_id = Model_Subject.module_id
 }
 // get all types
-Model_Subject.getAllTypes = (result) => {
+Model_Subject.getModule = (result) => {
   dbCon.query('SELECT sub_code, sub_name_thai, module_name FROM  subject INNER JOIN module ON  subject.module_id = module.module_id', (err, res) => {
     if(err) {
       console.log('Error while fetching subject types.', err)
@@ -17,3 +17,4 @@ Model_Subject.getAllTypes = (result) => {
     }
   })
 }
+module.exports = Model_Subject
