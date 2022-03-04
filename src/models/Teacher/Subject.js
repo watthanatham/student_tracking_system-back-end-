5a54632ee656870618b6d2297d499c32dcae014a
Watthanatham Kruram <61160045@go.buu.ac.th>
2022-03-04 15:04:51 +0700

    fixed key for query on subject

--- src/models/Teacher/Subject.js
+++ src/models/Teacher/Subject.js
@@ -3,7 +3,6 @@ var dbCon = require('../../../config/db.config')
 
 var Subject = function(subject) {
   this.sub_id = subject.sub_id
-  this.sub_code = subject.sub_code
   this.sub_name_thai = subject.sub_name_thai
   this.sub_name_eng = subject.sub_name_eng
   this.sub_credit = subject.sub_credit
@@ -13,7 +12,7 @@ var Subject = function(subject) {
 }
 // get all subjects
 Subject.getAllSubjects = (result) => {
-  dbCon.query('SELECT sub_id, sub_code, sub_name_thai, sub_name_eng, sub_credit, st_id, module_id FROM subject', (err, res) => {
+  dbCon.query('SELECT sub_id, sub_name_thai, sub_name_eng, sub_credit, st_name, module_name FROM subject INNER JOIN module ON subject.module_id = module.module_id INNER JOIN subject_type ON subject.st_id = subject_type.st_id', (err, res) => {
     if(err) {
       console.log('Error while fetching subjects', err)
       result(null, err)
@@ -25,7 +24,7 @@ Subject.getAllSubjects = (result) => {
 }
 // get subject by id 
 Subject.getSubjectById = (id, result) => {
-  dbCon.query('SELECT * FROM subject WHERE sub_id=?', id, (err, res) => {
+  dbCon.query('SELECT sub_id, sub_name_thai, sub_name_eng, sub_credit, st_name, module_name FROM subject INNER JOIN module ON subject.module_id = module.module_id INNER JOIN subject_type ON subject.st_id = subject_type.st_id WHERE sub_id=?', id, (err, res) => {
     if(err) {
       console.log('Error while fetching subject by id', err)
       result(null,err)
@@ -49,7 +48,7 @@ Subject.createNewSubject = (subjectReqData, result) => {
 }
 // update subject
 Subject.updateSubject = (id, subjectReqData, result) => {
-  dbCon.query('UPDATE subject SET sub_code=?, sub_name_thai=?, sub_name_eng=?, sub_credit=?, st_id=?, module_id=?, course_id=? WHERE sub_id=?', [subjectReqData.sub_code,subjectReqData.sub_name_thai,subjectReqData.sub_name_eng,subjectReqData.sub_credit,subjectReqData.st_id,subjectReqData.module_id, subjectReqData.course_id, id] , (err, res) => {
+  dbCon.query('UPDATE subject SET sub_name_thai=?, sub_name_eng=?, sub_credit=?, st_id=?, module_id=?, course_id=? WHERE sub_id=?', [subjectReqData.sub_name_thai,subjectReqData.sub_name_eng,subjectReqData.sub_credit,subjectReqData.st_id,subjectReqData.module_id, subjectReqData.course_id, id] , (err, res) => {
     if(err) {
       console.log('Error while update data')
       result(null, err)
