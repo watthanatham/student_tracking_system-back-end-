d36970f39a7f6190051075a2dc94a9ee934bb844
Watthanatham Kruram <61160045@go.buu.ac.th>
2022-03-07 14:46:32 +0700

    fixed sub_code to sub_id

--- src/models/Teacher/Type_Subject.js
+++ src/models/Teacher/Type_Subject.js
@@ -1,14 +1,14 @@
 var dbCon = require('../../../config/db.config')
 
 var Type_Subject = function(type_subject) {
-  this.sub_code = Type_Subject.sub_code
+  this.sub_id = Type_Subject.sub_id
   this.sub_name_thai = Type_Subject.sub_name_thai
   this.sub_credit = Type_Subject.sub_credit
   this.st_id = Type_Subject.st_id
 }
 // get all types
 Type_Subject.getTypeSubject = (result) => {
-  dbCon.query('SELECT sub_code, sub_name_thai, sub_credit, st_name FROM  subject INNER JOIN subject_type ON subject.st_id = subject_type.st_id', (err, res) => {
+  dbCon.query('SELECT sub_id, sub_name_thai, sub_credit, st_name FROM  subject INNER JOIN subject_type ON subject.st_id = subject_type.st_id', (err, res) => {
     if(err) {
       console.log('Error while fetching subject types.', err)
       result(null, err)
