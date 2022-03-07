8f3d094bef6d791d67f60a2f53a232453c9be944
Watthanatham Kruram <61160045@go.buu.ac.th>
2022-03-07 14:48:57 +0700

    fixed module sub_id

--- src/models/Teacher/Model_Subject.js
+++ src/models/Teacher/Model_Subject.js
@@ -1,13 +1,13 @@
 var dbCon = require('../../../config/db.config')
 
 var Model_Subject = function(model_subject) {
-  this.sub_code = Model_Subject.sub_code
+  this.sub_id = Model_Subject.sub_id
   this.sub_name_thai = Model_Subject.sub_name_thai
   this.module_id = Model_Subject.module_id
 }
 // get all types
 Model_Subject.getModule = (result) => {
-  dbCon.query('SELECT sub_code, sub_name_thai, module_name FROM  subject INNER JOIN module ON  subject.module_id = module.module_id', (err, res) => {
+  dbCon.query('SELECT sub_id, sub_name_thai, module_name FROM  subject INNER JOIN module ON  subject.module_id = module.module_id', (err, res) => {
     if(err) {
       console.log('Error while fetching subject types.', err)
       result(null, err)
